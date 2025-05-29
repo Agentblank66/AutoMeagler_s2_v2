6a32de53617aaa4b9f8d392077d8e8a4087df2c5
OKronborg00 <oliverkronborg00@gmail.com>
2025-05-29 22:13:13 +0200

    har tilføjet AdminCar page har ændrede index så vi har den gamle igen

---
+++ AutoMeagler_s2_v2/Pages/Cars/AdminCar.cshtml.cs
@@ -0,0 +1,24 @@
+using AutoMeagler_s2_v2.Models;
+using AutoMeagler_s2_v2.Service;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AutoMaegler.Pages.Cars
+{
+    public class AdminCarModel : PageModel
+    {
+        private readonly ICarService _carService;
+
+        public AdminCarModel(ICarService carService)
+        {
+            _carService = carService;
+        }
+
+        public List<Car> Cars { get; set; } = new();
+
+        public void OnGet()
+        {
+            Cars = _carService.GetCars();
+        }
+    }
+}
